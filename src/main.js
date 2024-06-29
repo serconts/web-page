2457aff686fd9a4fced5729e2b12be84a6cd3814
William Condori Quispe <williamccondori@accenti.io>
2024-06-29 18:17:28 -0500

    Add assets

--- src/main.js
+++ src/main.js
@@ -10,11 +10,11 @@ import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
 import './assets/vendor/bootstrap/css/bootstrap.min.css'
 import './assets/vendor/glightbox/css/glightbox.min.css'
 
-import './assets/css/style.css'
-
-
 import './assets/vendor/swiper/swiper-bundle.min.css'
 import './assets/vendor/swiper/swiper-bundle.min.js'
+
+import './assets/css/style.css'
+
 
 const app = createApp(App)
 
